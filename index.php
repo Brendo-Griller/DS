5fb5bc19443e77b633d49912173ba9d25c269355
Brendo <brendogriller@gmail.com>
2026-09-14 16:31:03 -0300

    verificando idade

--- index.php
+++ index.php
@@ -9,8 +9,17 @@
     <?php
     $nome= "Brendo";
     $idade= 30;
+     
+    if ($nome > 18){
+        $maior = "sim, é maior de idade";
+    }else{
+        $maior = "não, é de maior";
+
+    }
+
     ?>
     <h1>nome:<?= $nome?></h1>
     <p>idade:<?= $idade?></p>
+    <p>É maior de idade<?=$maior?></p>
 </body>
 </html>
