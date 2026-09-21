96fb6c16b125287ae0c437de272d8b2308e0a705
Brendo <brendogriller@gmail.com>
2026-09-21 17:05:24 -0300

    trocando nome placeholder

--- nota.php
+++ nota.php
@@ -46,23 +46,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br><br>
 
     <label for="nota1">Nota1</label>
-    <input type="number" id="nota1" name="nota1" min=0 max=10 placeholder="Digite sua idade">
+    <input type="number" id="nota1" name="nota1" min=0 max=10 placeholder="Digite sua Nota">
     <br><br>
 
     <label for="nota2">Nota2</label>
-    <input type="number" id="nota2" name="nota2" min=0 max=10 placeholder="Digite sua idade">
+    <input type="number" id="nota2" name="nota2" min=0 max=10 placeholder="Digite sua Nota">
     <br><br>
     
     <label for="nota3">Nota3</label>
-    <input type="number" id="nota3" name="nota3" min=0 max=10 placeholder="Digite sua idade">
+    <input type="number" id="nota3" name="nota3" min=0 max=10 placeholder="Digite sua Nota">
     <br><br>
 
     <label for="nota4">Nota4</label>
-    <input type="number" id="nota4" name="nota4" min=0 max=10 placeholder="Digite sua idade">
+    <input type="number" id="nota4" name="nota4" min=0 max=10 placeholder="Digite sua Nota">
     <br><br>
 
     <label for="nota5">Nota5</label>
-    <input type="number" id="nota5" name="nota5" min=0 max=10 placeholder="Digite sua idade">
+    <input type="number" id="nota5" name="nota5" min=0 max=10 placeholder="Digite sua Nota">
     <br><br>
     
     <button type="submit"> Enviar </button>
